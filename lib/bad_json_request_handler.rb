bfd9cce9b7e2e3331128425c3deedff92f67c5f8
Prathamesh Sonpatki <csonpatki@gmail.com>
2020-02-29 22:33:12 +0530

    Add middleware for catching bad JSON requests

--- lib/bad_json_request_handler.rb
+++ lib/bad_json_request_handler.rb
@@ -1,6 +1,25 @@
 require "bad_json_request_handler/version"
 
-module BadJsonRequestHandler
-  class Error < StandardError; end
-  # Your code goes here...
+class BadJsonRequestHandler
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    begin
+      @app.call(env)
+    rescue ActionDispatch::Http::Parameters::ParseError => error
+      error_output = "Invalid request payload: #{error}"
+
+      if env['CONTENT_TYPE'] =~ /application\/json/
+        return [
+          400,
+          { "Content-Type" => "application/json" },
+          { errors: { message: error_output } }.to_json
+        ]
+      else
+        raise error, error_output
+      end
+    end
+  end
 end
